594664fc4242d7515d946e923b1e67e99a7523be
fccview <f.campioneuk@gmail.com>
2025-08-11 13:58:54 +0100

    fixes and pwa support and docker compose

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const withPWA = require('next-pwa')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+    output: 'standalone',
 }
 
 module.exports = withPWA(nextConfig) 
